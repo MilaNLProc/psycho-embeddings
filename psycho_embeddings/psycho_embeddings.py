623701c1ddfde1cabfaaaf0a7a76cb92802d9975
Giuseppe Attanasio <giuseppe.attanasio@polito.it>
2022-03-04 09:14:37 +0000

    Add averaging of sub-word embeddings

--- psycho_embeddings/psycho_embeddings.py
+++ psycho_embeddings/psycho_embeddings.py
@@ -1,4 +1,15 @@
 from transformers import AutoTokenizer, AutoModel
+import torch
+
+
+def find_sub_list(sl, l):
+    results = list()
+    sll = len(sl)
+    for ind in (i for i, e in enumerate(l) if e == sl[0]):
+        if l[ind:ind + sll] == sl:
+            results.append((ind, ind + sll-1))
+
+    return results
 
 
 class BaseHFEmbedder:
@@ -8,11 +19,21 @@ class BaseHFEmbedder:
         self.tokenizer = None
 
     def get_single_embedding(self, sentence: str, word: str, layer: int):
-        tok = self.tokenizer([sentence], return_tensors="pt")
-        idx = self.tokenizer.convert_ids_to_tokens(list(tok["input_ids"][0])).index(word)
-
+        tok_sentence = self.tokenizer([sentence], return_tensors="pt")
+        tok_word = self.tokenizer(word, add_special_tokens=False)
+        
+        idx = find_sub_list(tok_word["input_ids"], tok_sentence["input_ids"][0].tolist())
+        idx = idx[0] # consider only the first occurrence
+        
+        with torch.no_grad():
+            outputs = self.model(**tok_sentence)
+        
         # index 0 is the first sentence (there's only one sentence in the list)
-        return self.model(**tok)["hidden_states"][layer][0][idx].detach().numpy()
+        target_emb = outputs["hidden_states"][layer][0][idx[0]:idx[1] + 1]
+        # average across subtokens
+        target_emb = target_emb.mean(0) 
+        
+        return target_emb.numpy()
 
 
 class GPT2Embedder(BaseHFEmbedder):
